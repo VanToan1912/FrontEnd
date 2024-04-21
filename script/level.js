0794019b2dc812b52f52ea7ce954be07a50b6578
VanToan1912 <21130572@st.hcmuaf.edu.vn>
2024-04-21 12:39:29 +0700

    123

--- script/level.js
+++ script/level.js
@@ -4,16 +4,16 @@
 let container = document.getElementById("container");
 let refreshbtn = document.getElementById("refresh");
 
-//board
+//màn hình game
 let board;
 let boardWidth = 500;
 let boardHeight = 650;
 let context;
 
-//players
-let playerWidth = 500; //500 for testing, 80 normal
+//người chơi
+let playerWidth = 80;
 let playerHeight = 10;
-let playerVelocityX = 10; //move 10 pixels each time
+let playerVelocityX = 10;
 
 let player = {
     x: boardWidth / 2 - playerWidth / 2,
@@ -23,11 +23,11 @@ let player = {
     velocityX: playerVelocityX
 }
 
-//ball
+//quả banh
 let ballWidth = 10;
 let ballHeight = 10;
-let ballVelocityX = 6; //15 for testing, 3 normal
-let ballVelocityY = 4; //10 for testing, 2 normal
+let ballVelocityX = 6; 
+let ballVelocityY = 4;
 
 let ball = {
     x: boardWidth / 2,
@@ -38,16 +38,15 @@ let ball = {
     velocityY: ballVelocityY
 }
 
-//blocks
+//mục tiêu
 let blockArray = [];
-let blockWidth = 500; //50
+let blockWidth = 500; 
 let blockHeight = 10;
-let blockColumns = 1; //8
-let blockRows = 1; //3 //add more as game goes on
-// let blockMaxRows = 10; //limit how many rows
+let blockColumns = 8; 
+let blockRows = 3; 
 let blockCount = 0;
 
-//starting block corners top left
+//mục tiêu đầu tiên sẽ được tạo ở góc trên bên trái
 let blockX = 15;
 let blockY = 45;
 
@@ -64,16 +63,16 @@ function level1() {
     board.style.display = "block";
     board.height = boardHeight;
     board.width = boardWidth;
-    context = board.getContext("2d"); //used for drawing on the board
+    context = board.getContext("2d");
 
-    //draw initial player
+    //vẽ người chơi trên canvas
     context.fillStyle = "skyblue";
     context.fillRect(player.x, player.y, player.width, player.height);
 
     requestAnimationFrame(update);
     document.addEventListener("mousemove", movePlayer);
 
-    //create blocks
+    //tạo tất cả mục tiêu
     createBlocks();
 }
 
@@ -91,17 +90,16 @@ let button6 = document.getElementById("btn-6");
 
 function update() {
     requestAnimationFrame(update);
-    //stop drawing
-    if (gameOver) {
+        if (gameOver) {
         return;
     }
     context.clearRect(0, 0, board.width, board.height);
 
-    // player
+    // vẽ người chơi
     context.fillStyle = "lightgreen";
     context.fillRect(player.x, player.y, player.width, player.height);
 
-    // ball
+    // vẽ quả banh
     context.fillStyle = "white";
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
@@ -115,7 +113,7 @@ function update() {
 
     handleBallBlockCollision();
 
-    //game ended
+    //Thắng game
     if (blockCount == 0) {
         context.font = "20px sans-serif";
         context.fillText("Bạn Đã Thắng! Quay Lại Màn Hình Chính", 80, 400);
@@ -127,7 +125,7 @@ function update() {
         }, 3000)
     }
 
-    //score
+    //Điểm
     context.font = "20px sans-serif";
     context.fillText(score, 10, 25);
 }
@@ -136,67 +134,68 @@ function outOfBounds(xPosition) {
     return (xPosition < 0 || xPosition + playerWidth > boardWidth);
 }
 
-// Update movePlayer to handle mouse movement
+// function update vị trí người chơi
 function movePlayer(e) {
     if (gameOver) {
         setTimeout(resetGame(), 3000);
         console.log("RESET");
         return;
     }
-    // Calculate the new player position based on mouse movement
+    // tính vị trí người chơi
     let mouseX = e.clientX - board.offsetLeft;
     let nextPlayerX = mouseX - player.width / 2;
-    // Ensure the player stays within the board boundaries
+    //giữ người chơi trong màn hình, không cho người chơi di chuyển khỏi màn hình
     if (nextPlayerX >= 0 && nextPlayerX <= boardWidth - player.width) {
         player.x = nextPlayerX;
     }
 }
 
 function detectCollision(a, b) {
-    return a.x < b.x + b.width && //a's top left corner doesn't reach b's top right corner
-        a.x + a.width > b.x && //a's top right corner passes b's top left corner
-        a.y < b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
-        a.y + a.height > b.y; //a's bottom left corner passes b's top left corner
+    return a.x < b.x + b.width && //góc trên bên trái của a đụng góc trên bên phải của b
+        a.x + a.width > b.x && //góc trên bên phải của a đụng góc trên bên trái của b
+        a.y < b.y + b.height && //góc dưới bên trái của a đụng góc dưới bên phải của 
+        a.y + a.height > b.y; //góc dưới bên phải của a đụng góc dưới bên trái của 
 }
 
 function handlePlayerBallCollision() {
     if (topCollision(ball, player) || bottomCollision(ball, player)) {
-        ball.velocityY *= -1; // flip y direction up or down
+        ball.velocityY *= -1; // đảo ngược chiều lên xuống của quả banh
     } else if (leftCollision(ball, player) || rightCollision(ball, player)) {
-        ball.velocityX *= -1; // flip x direction left or right
+        ball.velocityX *= -1; // đảo ngược chiều trái phải của quả banh
     }
 }
 
-function topCollision(ball, block) { //a is above b (ball is above block)
+function topCollision(ball, block) {
     return detectCollision(ball, block) && (ball.y + ball.height) >= block.y;
 }
 
-function bottomCollision(ball, block) { //a is above b (ball is below block)
+function bottomCollision(ball, block) {
     return detectCollision(ball, block) && (block.y + block.height) >= ball.y;
 }
 
-function leftCollision(ball, block) { //a is left of b (ball is left of block)
+function leftCollision(ball, block) {
     return detectCollision(ball, block) && (ball.x + ball.width) >= block.x;
 }
 
-function rightCollision(ball, block) { //a is right of b (ball is right of block)
+function rightCollision(ball, block) {
     return detectCollision(ball, block) && (block.x + block.width) >= ball.x;
 }
 
 function handleBallBoardCollision() {
     if (ball.y <= 0) {
-        // if ball touches top of canvas
-        ball.velocityY *= -1; //reverse direction
+        // nếu banh đụng cạnh trên của màn hình
+        ball.velocityY *= -1; //đảo ngược hướng
     } else if (ball.x <= 0 || (ball.x + ball.width >= boardWidth)) {
-        // if ball touches left or right of canvas
-        ball.velocityX *= -1; //reverse direction
+        // nếu banh đụng cạnh trái hoặc cạnh phải của màn hình
+        ball.velocityX *= -1; //đảo ngược hướng
     } else if (ball.y + ball.height >= boardHeight) {
-        // if ball touches bottom of canvas
+        // nếu banh đụng cạnh dưới của màn hình (thua cuộcc)
         handleGameOver();
     }
 }
 
 function handleBallBlockCollision() {
+    //nếu banh chạm mục tiêu, mục tiêu vỡ (ngưng vẽ mục tiêu) và cộng thêm điểm vào tổng điểm
     for (let i = 0; i < blockArray.length; i++) {
         let block = blockArray[i];
         if (!block.break && detectCollision(ball, block)) {
@@ -233,12 +232,12 @@ function drawBlocks() {
 }
 
 function createBlocks() {
-    blockArray = []; //clear blockArray
+    blockArray = []; //tạo mảng chứa các mục tiêu
     for (let c = 0; c < blockColumns; c++) {
         for (let r = 0; r < blockRows; r++) {
             let block = {
-                x: blockX + c * blockWidth + c * 10, //c*10 space 10 pixels apart columns
-                y: blockY + r * blockHeight + r * 10, //r*10 space 10 pixels apart rows
+                x: blockX + c * blockWidth + c * 10, //vị trí theo chiều dọc của mục tiêu là c*10, các cột mục tiêu cách nhau 10px
+                y: blockY + r * blockHeight + r * 10, //vị trí theo chiều ngang của mục tiêu là r*10, các hàng mục tiêu cách nhau 10px
                 width: blockWidth,
                 height: blockHeight,
                 break: false
